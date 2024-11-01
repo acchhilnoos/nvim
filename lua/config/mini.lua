781820dc1860c8b71e8b4f06707a857d35a7938c
acchhilnoos <1420436@gmail.com>
2024-10-31 21:11:19 -0700

    small changes to mini.starter

--- lua/config/mini.lua
+++ lua/config/mini.lua
@@ -9,6 +9,7 @@ return {
             items = {
                 starter.sections.recent_files(5, true, false),
                 starter.sections.recent_files(10, false, false),
+                { name = "Config",     action = "vi ~/.config/nvim",               section = "Actions" },
                 { name = "Finder",     action = "lua require('oil').open_float()", section = "Actions" },
                 { name = "Lazy",       action = "Lazy",                            section = "Actions" },
                 { name = "New buffer", action = "enew",                            section = "Actions" },
